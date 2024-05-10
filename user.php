29d1d753b37d2bd5cd25560f675d7d77ea66ab18
em-au <146881865+em-au@users.noreply.github.com>
2024-05-10 01:57:02 +0000

    Added create_user function (not functional)

--- user.php
+++ user.php
@@ -10,6 +10,14 @@ Class User {
     $rows = $statement->fetchAll(PDO::FETCH_ASSOC); // Retrieve the rows from the db; fetch into associative array
     return $rows;
   }
+
+  public function create_user($username, $password) {
+    $db = db_connect();
+    $statement = $db->prepare("INSERT into users...");
+    $statement->execute(); // Executes on filess.io end
+    $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+    return $rows; // Can return rows to make sure it's there
+  }
 }
 
 
