587af9dae850d91b070d6da221c23eafc0c3a50b
em-au <146881865+em-au@users.noreply.github.com>
2024-05-10 02:57:58 +0000

    Created signup.php

---
+++ signup.php
@@ -0,0 +1,46 @@
+<?php
+  session_start();
+  /*
+  // Check if user is authenticated (use session variable authenticated)
+  // If yes, redirect to index.php
+  if (isset($_SESSION['authenticated']) == 1) {
+    header ('location: /index.php');
+  }
+  */
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Sign up</title>
+  </head>
+  <body>
+
+    <h1>Sign Up Form</h1>
+
+    <form action="/validate.php" method="post">
+      <label for="username">Username:</label>
+      <br>
+      <input type="text" id="username" name="username">
+      <br>
+      <label for="password">Password:</label>
+      <br>
+      <input type="password" id="password" name="password">
+      <br>
+      <label for="password">Confirm Password:</label>
+      <br>
+      <input type="password" id="password" name="password">
+      <br><br>
+      <input type="submit" value="Submit">
+      <br><br>
+    </form>
+  </body>
+  
+  <!--
+  <footer>
+    <p><a href="/signup.php">Sign up</p>
+  </footer>
+  -->
+</html>
+
+
