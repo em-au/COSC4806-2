9717fce05044bc1e6d372034f523e6f73ad0c11d
em-au <146881865+em-au@users.noreply.github.com>
2024-05-10 01:29:02 +0000

    Created database.php

--- database.php
+++ database.php
@@ -2,5 +2,14 @@
 
 require_once('config.php');
 
+function db_connect() {
+  try {
+    $db = new PDO('mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbnamte=' . DB_DATABASE, DB_USER, DB_PASS);
+    return $dbh;
+  } catch (PDOException $e) {
+    // Should set a global variable here so we know that the DB is down
+    // session variable?
+  }
+
 
 ?>
