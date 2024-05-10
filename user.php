ad9df92806bb1155224dda5cd3b65b6acc3a3ba0
em-au <146881865+em-au@users.noreply.github.com>
2024-05-10 01:32:42 +0000

    Created user.php

---
+++ user.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once('database.php');
+
+Class User {
+  public function get_all_users() {
+    $db = db_connect();
+    $statement = $db->prepare("SELECT * FROM users");
+    $statement->execute(); // Executes on filess.io end
+    $rows = $statement->fetch(PDO::FETCH_ASSOC); // Retrieve the rows from the db; fetch into associative array
+    return $rows;
+  }
+}
+
+
+?>
