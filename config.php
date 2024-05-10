2139c609de74886daeb4af86df5494d644fd0da1
em-au <146881865+em-au@users.noreply.github.com>
2024-05-10 01:22:50 +0000

    Initial commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DB_HOST',      'mfz.h.filess.io');
+define('DB_USER',      'cosc4806_sleptrise');
+define('DB_PASS',      $_ENV['DB_PASS']);
+define('DB_DATABSE',    'cosc4806_sleptrise');
+define('DB_PORT',      '3305');
+
+?>
