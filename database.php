49a95d9d011ad56e6c41c414dbff85b62b9d8f55
em-au <146881865+em-au@users.noreply.github.com>
2024-05-10 01:44:28 +0000

    Fixed typos config.php and database.php
    Fixed typo in DB_DATABASE in config.php and in $dbh in database.php

--- database.php
+++ database.php
@@ -4,12 +4,12 @@ require_once('config.php');
 
 function db_connect() {
   try {
-    $db = new PDO('mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbnamte=' . DB_DATABASE, DB_USER, DB_PASS);
+    $dbh = new PDO('mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_DATABASE, DB_USER, DB_PASS);
     return $dbh;
   } catch (PDOException $e) {
     // Should set a global variable here so we know that the DB is down
     // session variable?
   }
-
+}
 
 ?>
