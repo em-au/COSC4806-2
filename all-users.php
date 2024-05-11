7bebba931159d05d674a8f758c751bd971da238e
em-au <146881865+em-au@users.noreply.github.com>
2024-05-11 00:30:37 +0000

    Made all-users.php accessible only to authenticated users

--- all-users.php
+++ all-users.php
@@ -1,20 +1,18 @@
 <?php
 
 require_once('user.php');
+session_start();
 
 // Check if user is authenticated
 // If not, redirect to login.php
-  if (isset($_SESSION['authenticated']) && ($_SESSION['authenticated']) != 1 || !isset($_SESSION['authenticated'])) {
-    header ('location: /login.php');
-  }
+if ((isset($_SESSION['authenticated']) && ($_SESSION['authenticated']) != 1) || !isset($_SESSION['authenticated'])) {
+  header ('location: /login.php');
+}
 
 $user = new User();
 $user_list = $user->get_all_users();
 
 echo "<pre>";
 print_r($user_list);
-
-$user = new User();
-$user->get_password("test");
 
 ?>
